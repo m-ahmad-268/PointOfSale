5ab4c4eea0a2ad4b60547113f0556644a2b64bdc
m-ahmad-268 <shamirsh0@gmail.com>
2025-08-08 11:11:01 +0500

    #POint_of_sale_ fixed sigu_fixes

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from 'react-redux';
